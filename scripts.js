a641e53dcb185b247e20439bf4d508177c107116
Jan Hoegerle <jan.hoegerle@gmail.com>
2025-06-03 23:24:27 +0200

    I guess this is my first design

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
 function openTwitch() {
   open("https://www.twitch.tv/doulrion", "_blank");
 }
+function openYoutube() {
+  open("https://www.youtube.com/@doulrion?sub_confirmation=1", "_blank");
+}
+function openDiscord() {
+  open("https://discord.com/invite/qucpGecNCC", "_blank");
+}
+function openTwitter() {
+  open("https://x.com/doulrion_jan", "_blank");
+}
+function openInsta() {
+  open("https://www.instagram.com/doulrion", "_blank");
+}
